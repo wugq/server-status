6e2165854ab916c7a8602d222719624223c614f4
wugq <wugq.dev@gmail.com>
2025-05-21 22:16:48 +1200

    add playground, add serial, print command

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -9,62 +9,76 @@ constexpr int redBtnPin = 11;
 constexpr int yellowBtnPin = 12;
 constexpr int ledPin = 13; // the number of the LED pin
 
+int commandIndex = 0;
+int commands[4] = {1, 2, 3, 4};
 
-int redBtnState = 0;
-int yellowBtnState = 0;
+int redBtnState = LOW;
+int yellowBtnState = LOW;
+int yellowBtnLastState = LOW;
+int yellowBtnPressTime = 0;
 
-unsigned long longPressAt = 0;
-bool isPressing = false;
-bool isLongPress = false;
-
+bool isPressingRed = false;
 bool isPressingYellow = false;
 
 DIYables_4Digit7Segment_74HC595 display(SCLK, RCLK, DIO);
 
+
+String readSerialCommand() {
+    if (Serial1.available() > 0) {
+        return Serial1.readStringUntil('\n');
+    }
+    return "";
+}
+
+void printCommand() {
+    int command = commands[yellowBtnPressTime % 4];
+    display.printInt(command, false);
+    Serial.println(yellowBtnPressTime);
+}
+
 void setup() {
+    Serial.begin(9600);
+    Serial1.begin(9600); // Connect to 0RX,1TX
+
     pinMode(redBtnPin, INPUT);
     pinMode(yellowBtnPin, INPUT);
     pinMode(ledPin, OUTPUT);
-    display.printFloat(-9.2, 2, false);
+
+    printCommand();
 }
 
-void loop() {
-    redBtnState = digitalRead(redBtnPin);
-    if (redBtnState == HIGH) {
-        if (!isPressing) {
-            longPressAt = millis();
-        }
-        isPressing = true;
-        isLongPress = (millis() - longPressAt) > 1000;
-    } else {
-        isPressing = false;
-        isLongPress = false;
-        longPressAt = 0;
-    }
 
-    if (isLongPress) {
-        display.printInt(2, false);
-    } else if (isPressing) {
-        display.printInt(1, false);
-    } else {
-        display.printInt(0, false);
+void loop() {
+    display.loop();
+
+    String command = readSerialCommand();
+    if (command.length() > 0) {
+        Serial.println(command);
+        commandIndex++;
+        commands[commandIndex % 4] = command.toInt();
+        printCommand();
     }
 
     yellowBtnState = digitalRead(yellowBtnPin);
     if (yellowBtnState == HIGH) {
         isPressingYellow = true;
-        digitalWrite(ledPin, HIGH);
     } else {
+        if (isPressingYellow) {
+            yellowBtnPressTime++;
+            printCommand();
+        }
         isPressingYellow = false;
-        digitalWrite(ledPin, LOW);
     }
 
-    if (isLongPress && isPressingYellow) {
-        display.printInt(12, false);
-    } else if (isPressingYellow && isPressing) {
-        display.printInt(11, false);
+    redBtnState = digitalRead(redBtnPin);
+    if (redBtnState == HIGH) {
+        isPressingRed = true;
+    } else {
+        isPressingRed = false;
     }
 
-
-    display.loop();
+    // display.setDot(1);
+    // display.setDot(2);
+    // display.setDot(3);
+    // display.setDot(4);
 }
