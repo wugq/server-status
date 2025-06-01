b4513242cbe97d9f97e9c0a36515b9edb91ddc59
wugq <wugq.dev@gmail.com>
2025-06-01 20:26:45 +1200

    run task

--- java-server/server-status-java-server/src/Main.java
+++ java-server/server-status-java-server/src/Main.java
@@ -1,8 +1,27 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        Runnable task = () -> {
+            System.out.println("Running task at " + java.time.LocalTime.now());
+            Main main = new Main();
+            main.sendMessage();
+        };
+
+        scheduler.scheduleAtFixedRate(task, 5, 10, TimeUnit.SECONDS);
+    }
+
+    private void sendMessage() {
         ServerInfo serverInfo = new ServerInfo();
 
         SerialConnection serialConnection = new SerialConnection("ttyUSB0", 9600);
-        serialConnection.send(serverInfo.collect().toJson());
+
+        ServerInfoDto serverInfoDto = serverInfo.collect();
+        System.out.println(serverInfoDto.toJson());
+        serialConnection.send(serverInfoDto.toJson());
     }
 }
